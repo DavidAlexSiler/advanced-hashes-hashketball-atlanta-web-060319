f7389b5f72785d7f108b3c6b13b12e5935a26f7a
Alex Siler <davidalexsiler@gmail.com>
2019-06-05 01:37:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,46 +119,96 @@ def game_hash
   }
 end
 
-def get_all_players
-  home_players = game_hash[:home][:players]
-  away_players = game_hash[:away][:players]
-  home_players.merge(away_players)
-end
-
-def find_stats_for_player(player_name)
-  get_all_players[player_name]
-end
-
-def shoe_size(player_name)
-  # get_all_players(player_name)[:shoe]
-  get_all_players[player_name][:shoe]
-
-  # stats = find_stats_for_player(player_name)
-  # stats[:shoe]
-end
-
-
-
-# def shoe_size(player_name)
-#   player_stats(player_name)[:shoe]
+# def get_all_players
+#   home_players = game_hash[:home][:players]
+#   away_players = game_hash[:away][:players]
+#   home_players.merge(away_players)
 # end
 
-def num_points_scored(player_name)
-  player_stats(player_name)[:points]
+# def find_stats_for_player(player_name)
+#   get_all_players[player_name]
+# end
+
+# def shoe_size(player_name)
+#   # get_all_players(player_name)[:shoe]
+#   get_all_players[player_name][:shoe]
+
+#   # stats = find_stats_for_player(player_name)
+#   # stats[:shoe]
+# end
+
+
+
+# # def shoe_size(player_name)
+# #   player_stats(player_name)[:shoe]
+# # end
+
+# def num_points_scored(player_name)
+#   player_stats(player_name)[:points]
+# end
+
+
+# def team_colors(team_name) 
+# game_hash.each do |team, keys|
+#   if keys[:team_name] == team_name
+#     return keys[:colors]
+#   end
+# end
+# end
+
+# def team_names
+#   game_hash.collect do |teams, keys|
+#     keys[:team_name]
+#   end
+# end
+    
+def num_points_scored(name)
+  player = find_the_player(name)
+  player.fetch(:points)
 end
 
+def shoe_size(name)
+  player = find_the_player(name)
+  player.fetch(:shoe)
+end
 
-def team_colors(team_name) 
- game_hash.each do |team, keys|
-   if keys[:team_name] == team_name
-     return keys[:colors]
-   end
- end
+def team_colors(team_name)
+  team = find_the_team(team_name)
+  team.fetch(:colors)
 end
 
 def team_names
-  game_hash.collect do |teams, keys|
-    keys[:team_name]
-  end
+  teams.map{|t| t.fetch(:team_name)}
 end
-    
+
+def player_numbers(team_name)
+  find_the_team(team_name)[:players].map{ |player_name, stats| stats[:number] }
+end
+
+def player_stats(player_name)
+  find_the_player(player_name)
+end
+
+def big_shoe_rebounds
+  player_biggest_shoe_size.fetch(:rebounds)
+end
+
+def teams
+  game_hash.values
+end
+
+def players
+  game_hash[:home][:players].merge(game_hash[:away][:players])
+end
+
+def find_the_team(team_name)
+  teams.find {|team| team.fetch(:team_name) == team_name}
+end
+
+def find_the_player(name)
+  players.fetch(name)
+end
+
+def player_biggest_shoe_size
+  players.max_by{|player, stats| stats.fetch(:shoe)}[1]
+end
